d13964d8a7a34a47ab0485cf87c6562a3eb16177
Naveen Chatta <naveenchatta1@gmail.com>
2023-12-19 21:33:28 +0530

    commiting my portfolio website

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Hello", "Welcome to Naveen portfolio website,from this web we can know about naveen skills,services and also personal information,you can contact him by sending message through website(click contact naveen) " , "Have a good day!"],
+    typeSpeed: 70,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
